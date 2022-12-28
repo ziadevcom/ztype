a51b4f6e96fb2354c859cd5f4e7e397d578f30a4
Zia Ahmad <iloveeidhi@gmail.com>
2022-12-29 01:27:09 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: { primary: "#323437", secondary: "#fda4af", white: "#ffffff" },
+    },
+    animation: {
+      "pulse-faster": "pulse 0.5s linear infinite",
+    },
+    fontFamily: {
+      josef: ["Josefin Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
